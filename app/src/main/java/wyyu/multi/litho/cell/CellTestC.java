fbb888bc8ab2b05a22266675cc68c32eb012892f
nanmeng <nanmeng2014@xiaochuankeji.cn>
2019-10-31 11:51:58 +0800

    FIX : LithoView

--- app/src/main/java/wyyu/multi/litho/cell/CellTestC.java
+++ app/src/main/java/wyyu/multi/litho/cell/CellTestC.java
@@ -11,6 +11,14 @@ import wyyu.multi.litho.widget.LithoContainerTest;
 
 /**
  * Created by wyyu on 2019-10-11.
+ *
+ * 使用 LithoView 在构建 ViewHolder 时，当 Adapter 调用 notifyItemRemoved 方法，屏幕外的 LithoView 没有正常加载
+ *
+ * 原因 ：Adapter 调用 notifyItemRemoved 时，回调到 RecyclerView 中，只做了 removeChildView 操作，不会触发
+ * offsetTopAndBottom 方法，屏幕外的 LithoView 就不会触发加载逻辑
+ *
+ * 解决方案 ：在调用 notifyItemRemoved 方法后，让 RecyclerView 调用 scrollBy 方法，给 RecyclerView 一个 1px 的位移，用来触发
+ * offsetTopAndBottom 方法，继而使屏幕外因 notifyItemRemoved 移入屏幕内的 LithoView 可正常绘制
  **/
 
 @BindCell(R.layout.layout_cell_litho_t) public class CellTestC implements IHolderCellWithCreate {
